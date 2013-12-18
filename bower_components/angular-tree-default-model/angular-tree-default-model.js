5a84dbf55629ead308fe867e8abf2e2e3a7fe3cf
Ruben Barilani <ruben.barilani.dev@gmail.com>
2013-12-18 12:01:17 +0100

    Built angular-tree-default-model gh-pages from commit 17e6cd4 on branch dev

--- bower_components/angular-tree-default-model/angular-tree-default-model.js
+++ bower_components/angular-tree-default-model/angular-tree-default-model.js
@@ -1,61 +1,45 @@
 /*!
- * angular-tree-default-model - v0.0.1-alpha - 2013-12-18 09:12 
+ * angular-tree-default-model - v0.0.1-alpha - 2013-12-18 11:12 
  * https://github.com/Treeweb/angular-tree-default-model 
  * Copyright (c) 2013  
  * Licensed WTFPL 
 */
-
-angular
-
-    .module('tree.default-model', ['ng'])
-
-    .directive('treeDefaultModel', [function () {
-        'use strict';
-
-        return {
-            require : 'ngModel',
-            restrict: 'A',
-            scope : {
-                treeDefaultModel : '=',
-                ngModel : '='
-            },
-            link: function (scope, element) {
-                var defaultModel = scope.treeDefaultModel;
-                var empty = scope.treeDefaultModelEmptyValue || '';
-
-                scope.$watch('treeDefaultModel' , function (newVal){
-                    if(!angular.isUndefined(newVal)) {
-
-                        if(defaultModel === scope.ngModel) {
-                            scope.ngModel = newVal;
-                        }
-
-                        defaultModel = newVal;
-                    }
-                });
-
-                element.bind('click', function (e) {
-                    scope.$apply(function () {
-
-                        e.preventDefault();
-
-                        if(scope.ngModel === scope.treeDefaultModel) {
-                            scope.ngModel = empty;
-                        }
-                    });
-                });
-
-                element.bind('blur', function (e) {
-                    scope.$apply(function () {
-
-                        e.preventDefault();
-
-                        if(scope.ngModel === empty) {
-                            scope.ngModel = defaultModel;
-                        }
-
-                    });
-                });
+angular.module('tree.default-model', ['ng']).directive('treeDefaultModel', [function () {
+    'use strict';
+    return {
+      require: 'ngModel',
+      restrict: 'A',
+      scope: {
+        treeDefaultModel: '=',
+        ngModel: '='
+      },
+      link: function (scope, element) {
+        var defaultModel = scope.treeDefaultModel;
+        var empty = scope.treeDefaultModelEmptyValue || '';
+        scope.$watch('treeDefaultModel', function (newVal) {
+          if (!angular.isUndefined(newVal)) {
+            if (defaultModel === scope.ngModel) {
+              scope.ngModel = newVal;
             }
-        };
-    }]);
+            defaultModel = newVal;
+          }
+        });
+        element.bind('click', function (e) {
+          scope.$apply(function () {
+            e.preventDefault();
+            if (scope.ngModel === scope.treeDefaultModel) {
+              scope.ngModel = empty;
+            }
+          });
+        });
+        element.bind('blur', function (e) {
+          scope.$apply(function () {
+            e.preventDefault();
+            if (scope.ngModel === empty) {
+              scope.ngModel = defaultModel;
+            }
+          });
+        });
+      }
+    };
+  }]);
